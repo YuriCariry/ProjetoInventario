52c7cb0226caeeebc79f69b194b998dd7d5312cc
Yuri Cariry <yuricariry@gmail.com>
2022-05-11 07:59:19 -0300

    Delete scripts directory

--- scripts/importar_csv.py
+++
@@ -1,9 +0,0 @@
-from AplicativoInventario.models import Inventario
-import csv
-
-def run():
-    f = open('C:\workspace\myenv\ProjetoInventario\scripts\inventario2022 _copia.csv', encoding='UTF8')
-    csv_reader = csv.reader(f)
-    for line in csv_reader:
-        inventario = Inventario(line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13])
-        inventario.save()
